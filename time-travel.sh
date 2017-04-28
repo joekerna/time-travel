e29b0bc418e5af894cc51d4d62a99e62aa29d016
Matthias Kern <matthias@Macbook.fritz.box>
2017-04-28 21:52:09 +0200

    Added Notification center messages

--- time-travel.sh
+++ time-travel.sh
@@ -3,14 +3,27 @@
 # -----------------------------------------------------------------------------
 # Variables
 # -----------------------------------------------------------------------------
-DESTINATION="$1"
-
-# -----------------------------------------------------------------------------
-# Run rsync-time-backup
-# -----------------------------------------------------------------------------
-./rsync-time-backup/rsync_tmbackup.sh /Users $DESTINATION
+SOURCE="$1"
+DESTINATION="$2"
+TMPDIR=/tmp
 
 # -----------------------------------------------------------------------------
 # Create notification-center popup
 # -----------------------------------------------------------------------------
-./terminal-notifier/terminal-notifier.app/Contents/MacOS/terminal-notifier -title "Time Travel" -message "Backup started..."
+./terminal-notifier.app/Contents/MacOS/terminal-notifier -title "Time Travel" -message "Backup started..."
+
+# -----------------------------------------------------------------------------
+# Run rsync-time-backup
+# -----------------------------------------------------------------------------
+./rsync-time-backup/rsync_tmbackup.sh --rsync-set-flags "-D --compress --numeric-ids --links --hard-links --one-file-system --itemize-changes --times --recursive --perms --owner --group --stats -h" $SOURCE $DESTINATION > $TMPDIR/time-travel.log
+
+# -----------------------------------------------------------------------------
+# Create notification-center popup
+# -----------------------------------------------------------------------------
+TOTALTRANSFERREDSIZE=$(grep 'Total transferred file size' $TMPDIR/time-travel.log)
+./terminal-notifier.app/Contents/MacOS/terminal-notifier -title "Time Travel" -message "$TOTALTRANSFERREDSIZE"
+
+# -----------------------------------------------------------------------------
+# Clean up
+# -----------------------------------------------------------------------------
+rm -rf $TMPDIR/time-travel.log
